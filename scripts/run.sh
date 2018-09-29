fe4cf6f7f1e90eb02678e8baf4d5103b99ee7740
Unknown <jibgilmon@yahoo.com>
2018-09-29 14:02:41 -0500

    Move install script to run script & use mainline package.json

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-if [ "$MYSQL_HOST" = "localhost" ] ||  [ "$MYSQL_HOST" = "127.0.0.1" ]; then
-	sh /app/install-mysql.sh
-fi
+
+set -e
+
+mysqld_safe &
+
+npm install npm@latest -g
+
+adduser -S cytube
+
+git clone -b 3.0 https://github.com/calzoneman/sync /home/cytube/app
+mkdir -p /home/cytube/certs
+cd /home/cytube/app
+sed -i 's/67c7c69a/ffdbce8/' package.json
+cp -f /scripts/config.docker.yaml /home/cytube/app
+chown -R cytube: /home/cytube
+
+su-exec cytube npm install
+su-exec cytube npm run build-server
 
 envsubst < config.docker.yaml > config.yaml
 while :
